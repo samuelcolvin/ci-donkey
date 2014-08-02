853b4e18d3ae01088a888bba45b29b9c88b5c7e5
Samuel Colvin <s@muelcolvin.com>
2014-08-02 15:26:24 +0100

    set FlaskCI.sh correct again

--- FlaskCI.sh
+++ FlaskCI.sh
@@ -2,10 +2,8 @@
 # <PRE SCRIPT>:
 virtualenv env
 echo "we haven't changed source so remember to manually call the correct python (or other script)"
-# env/bin/pip install -r requirements.txt
-#./env/bin/grablib grablib.json
-# this is should fail:
-grablib grablib.json
+env/bin/pip install -r requirements.txt
+./env/bin/grablib grablib.json
 
 # <MAIN SCRIPT>:
 # python manage.py test
